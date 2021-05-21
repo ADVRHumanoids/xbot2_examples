01a66e2149b8ad7c7320f8f53d3a522263c0bc5f
Francesco Iacobelli <framc2@gmail.com>
2021-05-21 19:22:15 +0200

    wip

--- src/cartesio/albero_cartesio_rt.cpp
+++ src/cartesio/albero_cartesio_rt.cpp
@@ -1,5 +1,6 @@
 #include "albero_cartesio_rt.h"
 
+
 using namespace XBot;
 using namespace XBot::Cartesian;
 
@@ -10,37 +11,40 @@ bool AlberoCartesioRt::on_initialize()
         return false;
     }
 
+    std::map<std::string, double> fc_map;
+
+    double high_fc = 0.99, low_fc = 0.80;
+    if (getParam("~fc_map", fc_map))
+    {
+        if (fc_map.count("high"))
+        fc_map.
+        fc_map.at("high")
+    }
+
     _robot->setControlMode(ControlMode::PosImpedance() + ControlMode::Effort());
 
     int dofs = _rt_model->getJointNum();
 
-    _k.setZero(dofs);
-    _d.setZero(dofs);
+    _start.init (dofs);
+    _torque.init(dofs);
+
+    _torque.d.update(0, true);
+    _torque.k.update(0, true);
+
+    return true;
 }
+
 
 void AlberoCartesioRt::starting()
 {
-    // _robot->sense(false);
+    /* in case of torque control set the joint impedance to zero */
 
-    // /* get the starting values of the joint impedance */
-
-    // _robot->getDamping(_d);
-    // _robot->getStiffness(_k);
-
-    // /* in case of torque control set the joint impedance to zero */
-
-    // if (_enable_feedback)
-    // {
-    //     _robot->setDamping(_k * 0.0);
-    //     _robot->setStiffness(_d * 0.0);
-    // }
-
-    // _robot->move();
-
-    // _robot->getDamping(_d);
-    // _robot->getStiffness(_k);
-
-    // XBOT2_INFO("stiffness:\n{}\ndamping:\n{}", _k, _d);
+    if (_enable_feedback)
+    {
+        _robot->setDamping  (_z);
+        _robot->setStiffness(_z);
+        _robot->move();
+    }
 
     CartesioRt::starting();
 }
@@ -48,12 +52,11 @@ void AlberoCartesioRt::starting()
 void AlberoCartesioRt::run()
 {
     CartesioRt::run();
-    
-    // _robot->getDamping(_d);
-    // _robot->getStiffness(_k);
 
-    XBOT2_INFO_EVERY(10s, "stiffness:\n{}\ndamping:\n{}", 3, 2);
+    XBOT2_INFO_EVERY(1s, "stiffness:\n{}\ndamping:\n{}", _k, _d);
 }
 
 XBOT2_REGISTER_PLUGIN(AlberoCartesioRt,
                       albero_cartesio_rt);
+
+#undef ZERO
--- src/cartesio/albero_cartesio_rt.h
+++ src/cartesio/albero_cartesio_rt.h
@@ -1,7 +1,11 @@
 #ifndef ALBERO_CARTESIO_RT_H
 #define ALBERO_CARTESIO_RT_H
 
+#define ZERO 1e-20
+
 #include "cartesio_rt.h"
+#include <ec_xbot2/joint_ec.h>
+#include <xbot2/robot_interface/robot_interface_xbot_rt.h>
 
 namespace XBot {
 
@@ -16,6 +20,7 @@ public:
     void starting() override;
     void run() override;
     
+    // void on_start() override;
     // void stopping() override;
     // void on_abort() override;
     // void on_close() override;
@@ -24,14 +29,158 @@ private:
 
     /* data members */
 
-    Eigen::VectorXd _k, _d;
+    profile _at_start, _at_run;
 
     /* methods */
     
     void updateModel();
+
+    /* utils */
+
 };
 
+class profile
+{
+    private:
+
+        typedef Eigen::VectorXd vector;
+        typedef RobotInterfaceXBot2Rt::Ptr robot_ptr;
+        
+        struct vectorial_item
+        {
+            bool update;
+            vector value;
+            
+            /* constructors */
+
+            vectorial_item() {}
+
+            init(uint dofs, bool upd = false)
+            {
+                update = upd;
+                value.setZero(dofs);
+            }
+
+            init(vector& val, bool upd = false)
+            {
+                update = upd;
+                value = val;
+            }
+
+            init(uint dofs, double val, bool upd = false)
+            {
+                update = upd;
+                value.setConstant(dofs, val);
+            }
+        };
+
+        /* data members */
+        vector current, diff;
+        
+    public:
+        
+        /* data members */
+
+        vectorial_item q, dq, k, d, fc, t;
+
+        /* methods */
+        
+        profile() {}
+
+        init(const uint dof)
+            : q(dof), dq(dof), k(dof), d(dof), fc(dof), t(dof)
+        {
+            diff.setZero(dof); current.setZero(dof);
+            q.init(dof); dq.init(dof); k.init(dof); d.init(dof); fc.init(dof); t.init(dof);
+        }
+        
+        void update_robot(robot_ptr robot)
+        {
+            /* TODO: let's remove duplicated code */
+
+            if (d.update) {
+                robot->setDamping(d.value);
+            }
+
+            if (k.update) {
+                robot->setStiffness(k.value);
+            }
+
+            if (q.update) {
+                robot->setPositionReference(q.value);
+            }
+            
+            if (dq.update) {
+                robot->setPositionReference(dq.value);
+            }
+            
+            if (fc.update) {
+                double fc_target = fc.value(0);
+                auto ec_joints = robot->getDevices<Hal::JointEc>();
+
+                for (auto joint : ec_joints.get_device_vector()) {
+                    /* kiss: now we just set the same fc for all the fc joints */
+                    joint->setFrictionCompensation(fc_target);
+                }
+            }
+        }
+
+        bool reached(robot_ptr robot)
+        {
+            /* TODO: let's remove duplicated code */
+
+            bool check = true;
+
+            if (d.update)
+            {
+                robot->getDamping(current);
+                diff = current - d.value;
+                
+                check &= diff.norm() < ZERO;
+            }
+
+            if (k.update)
+            {
+                robot->getStiffness(current);
+                diff = current - k.value;
+
+                check &= diff.norm() < ZERO;
+            }
+
+            if (q.update)
+            {
+                robot->getPositionReference(current);                
+                diff = current - q.value;
+                
+                check &= diff.norm() < ZERO;
+            }
+            
+            if (dq.update)
+            {
+                robot->getVelocityReference(current);
+                diff = current - dq.value;
+
+                check &= diff.norm() < ZERO;
+            }
+            
+            if (fc.update)
+            {
+                double fc_target = fc.value(0);
+                auto ec_joints = robot->getDevices<Hal::JointEc>();
+
+                for (auto joint : ec_joints.get_device_vector()) {
+                    /* kiss: now we just set the same fc for all the fc joints */
+                    double fc_current = joint->getFrictionCompensation();
+
+                    check &= abs(fc_current - fc_target) < ZERO;
+                }
+            }
+
+            return check;
+        }
+};
 
 }
 
+#undef ZERO
 #endif // CARTESIO_RT_H

d5b0ec955218a68d4545d7d822881800dfd685c8
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-10-27 17:28:25 +0200

    works with nvidia-docker

--- docker/run.sh
+++ docker/run.sh
@@ -2,26 +2,21 @@
 
 set -e
 
-# get path to script and change working directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/..
-
-docker run -it \
- --env="DISPLAY" \
- --env="QT_X11_NO_MITSHM=1" \
+nvidia-docker run --rm -it \
  --device=/dev/dri:/dev/dri \
+ --privileged \
+ --env "DOCKER=1" \
+ --env="DISPLAY=$DISPLAY" \
  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+ --volume="$HOME/.ssh:/home/user/.ssh:ro" \
+ --env="XAUTHORITY=$XAUTH" \
+ --volume="$XAUTH:$XAUTH" \
  --name xbot2examples \
  --publish 8080:8080 \
+ --publish 8888:8888 \
  -v $(pwd)/src:/home/user/src/xbot2_examples/src \
  -v $(pwd)/world:/home/user/src/xbot2_examples/world \
  -v $(pwd)/config:/home/user/src/xbot2_examples/config \
  -v $(pwd)/CMakeLists.txt:/home/user/src/xbot2_examples/CMakeLists.txt \
  arturolaurenzi/xbot2:examples bash
-#
-#-v $(pwd)/world:/home/user/src/xbot2_examples/world \
-#-v $(pwd)/config:/home/user/src/xbot2_examples/config \ 
-#-v $(pwd)/CMakeLists.txt:/home/user/src/xbot2_examples/CMakeLists.txt \
-
-
 

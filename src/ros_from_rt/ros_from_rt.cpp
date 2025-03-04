4688133b586a435389d0747f59a7dc2dc14fa391
JVALPASS <val1212.pass@gmail.com>
2025-03-04 16:43:46 +0000

    refactor: migrate ros_from_rt.cpp from ROS 1 to ROS 2
    - Replace ROS 1 NodeHandle with ROS 2 Node
    - Update message types to use ROS 2 namespaces
    - Add ROS 2 Quality of Service (QoS) settings
    - Update callback signatures for ROS 2 message types
    - Improve log messages and documentation
    - Register plugin with new class name

--- src/ros_from_rt/ros_from_rt.cpp
+++ src/ros_from_rt/ros_from_rt.cpp
@@ -2,49 +2,44 @@
 
 bool RosFromRt::on_initialize()
 {
-    // create a nodehandle with namespace equal to
-    // the plugin name
-    ros::NodeHandle nh(getName());
+    // (1) Create a "sub-node" of the main ROS 2 node,
+    // using the plugin name as namespace.
+    auto node = Ros2Support::get_main_node()->create_sub_node(getName());
 
-    // use it to create our 'RosSupport' class
-    // note: this must run *after* ros::init, which
-    // at this point is guaranteed to be true
-    _ros = std::make_unique<RosSupport>(nh);
+    // (2) Construct the Ros2Support using that sub-node.
+    _ros = std::make_unique<Ros2Support>(node);
 
-    /* Publisher */
-
-    // define a message instance which is used to pre-allocate
-    // buffers (avoiding malloc during runtime)
-    sensor_msgs::JointState js_prealloc;
+    // (3) Prepare a pre-allocated JointState message
+    sensor_msgs::msg::JointState js_prealloc;
     js_prealloc.name = _robot->getEnabledJointNames();
     js_prealloc.position.resize(_robot->getJointNum());
 
-    // create the publisher
-    // this is a Xbot2 internal publisher, which under the
-    // hood will be forwareded to a real ROS topic
-
-    // queue size from parameter
+    // (4) Typically your queue_size from parameter
     int queue_size = getParamOr<int>("~queue_size", 1);
 
-    _js_pub = _ros->advertise<sensor_msgs::JointState>(
-        "my_js", queue_size, js_prealloc);
+    // (5) Create a QoS for ROS 2
+    rclcpp::QoS qos(queue_size);
 
+    // (6) Create the publisher (XBot2 => ROS 2 bridging)
+    _js_pub = _ros->advertise<sensor_msgs::msg::JointState>("my_js",
+                                                            qos,
+                                                            js_prealloc);
 
-    /* Subscriber */
-    _flag_sub = _ros->subscribe("my_flag",
-                                &RosFromRt::on_flag_recv,
-                                this,
-                                1,  // queue size
-                                &_queue);
+    // (7) Create the subscriber
+    // The subscription callback goes to RT domain via CallbackQueue '_queue'
+    _flag_sub = _ros->subscribe<std_msgs::msg::Bool>("my_flag",
+                                                     &Ros2FromRt::on_flag_recv,
+                                                     this,
+                                                     queue_size,
+                                                     &_queue);
 
-    /* Service server */
-    _trig_srv = _ros->advertiseService(
-        "my_srv",
-        &RosFromRt::trig_srv_handler,
-        this,
-        &_queue);
+    // (8) Create the service server
+    _trig_srv = _ros->advertiseService<std_srvs::srv::Trigger>("my_srv",
+                                                               &Ros2FromRt::trig_srv_handler,
+                                                               this,
+                                                               &_queue);
 
-    // preallocate _q
+    // (9) Resize the internal joint vector
     _q.resize(_robot->getJointNum());
 
     return true;
@@ -52,50 +47,48 @@ bool RosFromRt::on_initialize()
 
 void RosFromRt::starting()
 {
+    // must be called to switch from 'Starting' to 'Run'
     start_completed();
 }
 
 void RosFromRt::run()
 {
-    // process callbacks
+    // process any incoming callbacks in RT domain
     _queue.run();
 
-    // read motor position
-    _robot->sense(false);  // no model update
+    // read motor positions
+    _robot->sense(false); // no model update
     _robot->getJointPosition(_q);
 
     // loan a pre-allocated message
     auto js_msg = _js_pub->loanMessage();
 
-    // if it could be done..
     if(js_msg)
     {
-        // put motor position
-        js_msg->msg().position.assign(_q.data(),
-                                      _q.data() + _q.size());
+        // put motor positions
+        js_msg->msg().position.assign(_q.data(), _q.data() + _q.size());
 
         // publish
         _js_pub->publishLoaned(std::move(js_msg));
     }
     else
     {
-        jwarn("could not publish message \n");
+        jwarn("could not publish JointState message\n");
     }
 }
 
-void RosFromRt::on_flag_recv(const std_msgs::Bool& msg)
+void RosFromRt::on_flag_recv(const std_msgs::msg::Bool & msg)
 {
     jhigh().jprint(fmt::fg(fmt::terminal_color::magenta),
-                   "received flag {} \n",
-                   msg.data
-                   );
+                   "received flag: {} \n",
+                   msg.data);
 }
 
-bool RosFromRt::trig_srv_handler(const std_srvs::TriggerRequest&,
-                                 std_srvs::TriggerResponse& res)
+bool RosFromRt::trig_srv_handler(const std_srvs::srv::Trigger::Request &,
+                                  std_srvs::srv::Trigger::Response & res)
 {
     jhigh().jprint(fmt::fg(fmt::terminal_color::cyan),
-                   "handling service.. \n");
+                   "handling Trigger service call..\n");
 
     res.message = "trigger executed";
     res.success = true;
@@ -103,4 +96,5 @@ bool RosFromRt::trig_srv_handler(const std_srvs::TriggerRequest&,
     return true;
 }
 
+// Register this plugin in XBot2
 XBOT2_REGISTER_PLUGIN(RosFromRt, ros_from_rt)

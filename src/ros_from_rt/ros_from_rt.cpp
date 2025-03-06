4d8bc0dfc2fc3be2b82da80a7c4d0bc293bee467
JVALPASS <val1212.pass@gmail.com>
2025-03-06 16:54:20 +0000

    refactor: Migrate ros_from_rt to ROS2
    - Rename class from RosFromRt to Ros2FromRt
    - Update API calls to use ROS2 patterns and QoS
    - Replace getEnabledJointNames() with getJointNames()
    - Implement ROS2 Quality of Service using KeepLast policy
    - Temporarily comment out service server due to template compatibility issues
    - Update plugin registration to use Ros2FromRt class
    - Fix callback signatures for ROS2 messaging system

--- src/ros_from_rt/ros_from_rt.cpp
+++ src/ros_from_rt/ros_from_rt.cpp
@@ -1,57 +1,61 @@
 #include "ros_from_rt.h"
 
-bool RosFromRt::on_initialize()
-{
+bool Ros2FromRt::on_initialize() {
     // (1) Create a "sub-node" of the main ROS 2 node,
     // using the plugin name as namespace.
     auto node = Ros2Support::get_main_node()->create_sub_node(getName());
-
+    
     // (2) Construct the Ros2Support using that sub-node.
     _ros = std::make_unique<Ros2Support>(node);
-
+    
     // (3) Prepare a pre-allocated JointState message
     sensor_msgs::msg::JointState js_prealloc;
-    js_prealloc.name = _robot->getEnabledJointNames();
+    js_prealloc.name = _robot->getJointNames();
     js_prealloc.position.resize(_robot->getJointNum());
-
+    
     // (4) Typically your queue_size from parameter
     int queue_size = getParamOr<int>("~queue_size", 1);
-
+    
     // (5) Create a QoS for ROS 2
-    rclcpp::QoS qos(queue_size);
-
+    rclcpp::KeepLast kl_policy(queue_size);
+    rclcpp::QoS qos(kl_policy);
+    
     // (6) Create the publisher (XBot2 => ROS 2 bridging)
     _js_pub = _ros->advertise<sensor_msgs::msg::JointState>("my_js",
-                                                            qos,
-                                                            js_prealloc);
-
+                                                             qos,
+                                                             js_prealloc);
+    
     // (7) Create the subscriber
     // The subscription callback goes to RT domain via CallbackQueue '_queue'
     _flag_sub = _ros->subscribe<std_msgs::msg::Bool>("my_flag",
-                                                     &Ros2FromRt::on_flag_recv,
-                                                     this,
-                                                     queue_size,
-                                                     &_queue);
-
-    // (8) Create the service server
+                                                      &Ros2FromRt::on_flag_recv,
+                                                      this,
+                                                      queue_size,
+                                                      &_queue);
+    
+    // (8) Create the service server - COMMENTED OUT DUE TO TEMPLATE ISSUE
+    // TODO: Fix this when the ROS 2 support library is updated
+    /*
     _trig_srv = _ros->advertiseService<std_srvs::srv::Trigger>("my_srv",
-                                                               &Ros2FromRt::trig_srv_handler,
-                                                               this,
-                                                               &_queue);
-
+                                                                &Ros2FromRt::trig_srv_handler,
+                                                                this,
+                                                                &_queue);
+    */
+    
     // (9) Resize the internal joint vector
     _q.resize(_robot->getJointNum());
-
+    
     return true;
 }
 
-void RosFromRt::starting()
+
+void Ros2FromRt::starting()
 {
     // must be called to switch from 'Starting' to 'Run'
     start_completed();
 }
 
-void RosFromRt::run()
+void Ros2FromRt::run()
 {
     // process any incoming callbacks in RT domain
     _queue.run();
@@ -77,14 +81,14 @@ void RosFromRt::run()
     }
 }
 
-void RosFromRt::on_flag_recv(const std_msgs::msg::Bool & msg)
+void Ros2FromRt::on_flag_recv(const std_msgs::msg::Bool & msg)
 {
     jhigh().jprint(fmt::fg(fmt::terminal_color::magenta),
                    "received flag: {} \n",
                    msg.data);
 }
 
-bool RosFromRt::trig_srv_handler(const std_srvs::srv::Trigger::Request &,
+bool Ros2FromRt::trig_srv_handler(const std_srvs::srv::Trigger::Request &,
                                   std_srvs::srv::Trigger::Response & res)
 {
     jhigh().jprint(fmt::fg(fmt::terminal_color::cyan),
@@ -97,4 +101,4 @@ bool RosFromRt::trig_srv_handler(const std_srvs::srv::Trigger::Request &,
 }
 
 // Register this plugin in XBot2
-XBOT2_REGISTER_PLUGIN(RosFromRt, ros_from_rt)
+XBOT2_REGISTER_PLUGIN(Ros2FromRt, ros_from_rt)
